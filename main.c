e28681b571461675c061edc0d969c24dc857d0b6
Viccini Patricio <pato98115@gmail.com>
2019-11-27 14:56:22 -0300

    revision de informe y eliminacion de codigo viejo

--- main.c
+++ main.c
@@ -1,19 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*! Estructura del bloque de meta-data */
-struct s_block {
-    size_t    size;
-    struct    s_block *prev;
-    struct    s_block *next;
-    int       free;
-    void      *ptr;
-    char      data[1];
-};
-typedef struct s_block *t_block ;
-
-/* Define the block size since the sizeof will be wrong*/
-# define BLOCK_SIZE sizeof(struct s_block) //20 //sizeof(struct s_block) - 4
+/*! META_SIZE es el tamaño del bloque de meta-data */
+#define META_SIZE sizeof(struct metadata_block)
 
 /*! Para alinear punteros, se utilizan tamaños muliplos de 32 bits (4 bytes). Se usan operadores bitwise */
 #define align4(x) (((((x)-1)>>2)<<2)+4)
@@ -22,17 +11,24 @@ typedef struct s_block *t_block ;
 void* malloc(size_t size);
 
 /*! Declaración de la función free() */
-void free(void *ptr);
+void liberar_bloque(void *ptr);
 
 /*! Declaración de funciones auxiliares */
-t_block get_block (void *p);
-int valid_addr (void *p);
-void split_block ( t_block b, size_t s);
-t_block fusion (t_block b);
-t_block find_block(t_block *last , size_t size );
-t_block extend_heap ( t_block last , size_t s);
+struct metadata_block* encontrar_bloque_libre(struct metadata_block** last, size_t size);
+struct metadata_block* extender_heap(struct metadata_block* last, size_t size);
+struct metadata_block* obtener_puntero_bloque(void *ptr);
+struct metadata_block* fusionar_bloques (struct metadata_block* block);
+void dividir_bloque(struct metadata_block* block, size_t size);
+void imprimir_lista(void);
 
-void print_list(void);
+/*! Estructura del bloque de meta-data */
+struct metadata_block {
+  size_t size;
+  struct metadata_block *prev;
+  struct metadata_block *next;
+  int free;
+  char data[1];
+};
 
 /* Inicio de la lista doblemente enlazada */
 static void* base = NULL;
@@ -53,286 +49,247 @@ int main(int argc, char *argv[]){
   void *ptr5 = malloc(250);
 
   printf("Luego de alocarlos, la lista que representa el heap queda asi:\n");
-  print_list();
+  imprimir_lista();
 
   printf("\n");
   printf("Libero el bloque de 200 con free(), la tabla queda como la siguiente:\n");
-  free(ptr2);
-  print_list();
+  liberar_bloque(ptr2);
+  imprimir_lista();
 
   printf("\n");
   printf("Aloco memoria con malloc(80), se reutiliza el espacio vacio y se divide el bloque. \n");
   void *ptr6 = malloc(80);
-  print_list();
+  imprimir_lista();
 
   printf("\n");
   printf("Liberamos el bloque que habiamos alocado con malloc(70), y vemos que se va a unir con \n");
   printf("el bloque libre anterior, que surgió de alocar en un espacio libre mas grande. \n");
-  free(ptr3);
-  print_list();
+  liberar_bloque(ptr3);
+  imprimir_lista();
 
   printf("\n");
   printf("Si necesitamos más espacio que el que hay libre en algún bloque, se agranda el break. \n");
   void *ptr7 = malloc(256);
-  print_list();
+  imprimir_lista();
 
   printf("\n");
   printf("Si libero todo lo ocupado.\n");
-  free(ptr1);
-  free(ptr4);
-  free(ptr5);
-  free(ptr6);
-  free(ptr7);
-  print_list();
-
+  liberar_bloque(ptr1);
+  liberar_bloque(ptr4);
+  liberar_bloque(ptr5);
+  liberar_bloque(ptr6);
+  liberar_bloque(ptr7);
+  imprimir_lista();
   printf("\n");
-  printf("Alloco de nuevo con malloc(100), se reutiliza lo anteriormente alocado, y se divide.\n");
-  void *ptr8 = malloc(100);
-  print_list();
-
+  printf("Alloco de nuevo con malloc(90), se reutiliza lo anteriormente alocado, y se divide.\n");
+  void *ptr8 = malloc(90);
+  imprimir_lista();
     return 0;
 }
 
 /* Función malloc() */
 void *malloc(size_t size) {
-    t_block bloque,last;
-    size_t s;
-    s = align4 (size);
+  struct metadata_block *block;
 
-    if (base != NULL) {
+  /* Alineo el tamaño requerido al múltiplo de 4 más cercano */
+  size = align4(size);
 
-       // printf("malloc1 \n"); //************************************
+  /* Controlo que el tamaño sea positivo */
+  if (size <= 0) {
+    return NULL;
+  }
 
-        /* First find a block */
-        last = base;
-        bloque = find_block(&last ,s);
-        if (bloque) {
 
-           // printf("malloc2 \n"); //************************************
-
-        /* can we split */
-            if ((bloque->size - s) >= ( BLOCK_SIZE + 4)){
-
-                //printf("malloc3 \n"); //************************************
-
-                split_block (bloque,s);
-            }
-            bloque->free =0;
-        } 
-        else {
-
-            //printf("malloc4 \n"); //************************************
-
-            /* No fitting block , extend the heap */
-            bloque = extend_heap (last ,s);
-            if (!bloque){
-
-                //printf("malloc5 \n"); //************************************
-
-                return (NULL);
-            }
-        }
-    } 
-    else {
-
-       // printf("malloc6 \n"); //************************************
-
-        /* first time */
-        bloque = extend_heap (NULL ,s);
-
-        if (!bloque){
-            return (NULL);
-        }
-        base = bloque;
-        //printf("holaaaaa\n");
+  if (!base) {
+    /* Si base es NULL, es la primera vez que se llama a malloc() */
+    /* Como es la primera vez, llamo a extender_heap */
+    block = extender_heap(NULL, size);
+    if (!block) {
+      /* Si ocurrio algún error, salgo */
+      return NULL;
     }
-    return (bloque->data);
-}
+    /* Coloco al nuevo bloque como la base de la lista */
+    base = block;
+  } else {
+    /* Si no es la primera vez que se llama malloc() */
 
+    /* Veo si hay un bloque libre del tamaño requerido o mayor */
+    /* Para esto, recorro la lista desde el principio (First Fit) */
+    struct metadata_block *last = base;
+    block = encontrar_bloque_libre(&last, size);
+    if (!block) {
+      /* Si no se encontro nigún bloque, llamo a extender_heap */
+      block = extender_heap(last, size);
+      if (!block) {
+        /* Si hubo un error, salgo de la función */
+        return NULL;
+      }
+    } else {
+        /* Si se encontro un bloque libre, me fijo que sea del tamaño necesario */
+        if((block->size - size) >= META_SIZE + 4) {
+          /* Y divido, tomo el espacio que necesito y armo otro bloque con el resto */
+          dividir_bloque(block, size);
+
+        }
+
+        /* Pongo el bloque que estaba libre, como ocupado */
+        block->free = 0;
+    }
+  }
+  //printf("sale del malloc\n");
+  return(block + 1);
+}
 
 /* Función free() */
-void free(void *p){
+void liberar_bloque(void *ptr) {
+  /* Controlo que el puntero pasado no sea NULL */
+  if (!ptr) {
+    return;
+  }
 
-    //printf("\n %10p \n\n", p);//************************!!!!!!
+  /* Obtengo el puntero al bloque apuntado por "ptr" */
+  struct metadata_block* block = obtener_puntero_bloque(ptr);
 
-    t_block b;
-    if (valid_addr(p)){
+  /* Marco el bloque como libre */
+  block->free = 1;
 
-        //printf("aca1 \n");//************************!!!!!!
+  /* Veo si hay bloques libres antes o después, intento fusionarlos */
+  if(block->prev && block->prev->free) {
+    block = fusionar_bloques(block->prev);
+  }
 
-        b = get_block(p);
-        b->free = 1;
-        /* fusion with previous if possible */
-        if(b->prev && b->prev->free){
-
-            //printf("aca2 \n");//************************!!!!!!
-
-            b = fusion (b->prev );
-        }
-        /* then fusion with next */
-        if (b->next){
-        fusion (b);
-        }
-        else{
-            /* free the end of the heap */
-
-            //printf("aca3 \n");//************************!!!!!!
-
-            if (b->prev){
-                b->prev->next = NULL;
-
-               // printf("aca4 \n");//************************!!!!!!
-
-            
-            }
-            else{
-                /* No more block !*/
-
-                //printf("aca5 \n");//************************!!!!!!
-
-            
-                base = NULL;
-            }
-            brk(b);
-        }
+  if(block->next) {
+    block = fusionar_bloques(block);
+  } else {
+    /* Se llego al final de la lista */
+    if (block->prev) {
+      /* Pongo al último bloque como libre */
+      block->free = 1;
     }
-    else{
-
-        //printf("aca6 \n");//************************!!!!!!
-
-    }
-}
-/* Get the block from and addr */
-t_block get_block (void *p) {
-    char *tmp;
-    tmp = p;
-    tmp = tmp - BLOCK_SIZE;
-    //t_block t = (t_block) tmp;
-    return (t_block)tmp;
-    }
-
-/* Valid addr for free*/
-int valid_addr (void *p){
-    if (base){
-
-            //printf("aca7 \n");//************************!!!!!!
-
-            
-        if ( p>base && p< sbrk(0)){
-
-
-            //printf("aca8 \n");//************************!!!!!!
-
-
-            return (p == ( get_block(p))->ptr );
-        }
-    }
-
-            //printf("aca9 \n");//************************!!!!!!
-
-            
-    return (0);
+  }
 }
 
 /* Función split_block() */
-/* Split block according to size.*/
-/* The b block must exist */
-void split_block ( t_block b, size_t s){
-    t_block new;
-    new = ( t_block )(b->data + s);
-    new->size = b->size - s - BLOCK_SIZE ;
-    new->next = b->next;
-    new->prev = b;
-    new->free = 1;
-    new->ptr = new->data;
-    b->size = s;
-    b->next = new;
-    if (new->next){
-        new->next->prev = new;
-    }
-    return;
+void dividir_bloque(struct metadata_block* block, size_t size) {
+  /*Guardo el tamaño total del bloque actual*/
+  size_t total_size = block->size;
+
+  /* Creo un bloque nuevo, que comience despues que
+     termine la primera parte del bloque dividido */
+  struct metadata_block* nuevo = (struct metadata_block* ) block->data + size;
+
+  /* Si el bloque tenia un bloque siguiente */
+  if(block->next) {
+    /* Pongo el siguiente del nuevo, como el siguiente del anterior(lo pongo entre medio) */
+    nuevo->next = block->next;
+    /* Y el anterior del siguiente del bloque como el nuevo (el bloque de adelante tiene como anterior al bloque nuevo) */
+    nuevo->next->prev = nuevo;
+  }
+
+  /* El nuevo tamaño del bloque "block" es size */
+  block->size = size;
+  /* Y el siguiente bloque es el "nuevo" */
+  block->next = nuevo;
+
+  /* El tamaño del nuevo es el tamaño total del anterior menos el tamaño
+     del bloque, y el tamaño de la memoria utilizada en la primera division */
+  nuevo->size = total_size - size - META_SIZE;
+
+  /* Pongo al anterior del nuevo, como el original */
+  nuevo->prev = block;
+  /* Y lo marco como libre */
+  nuevo->free = 1;
 }
 
 /* Función fusionar_bloques() */
-t_block fusion (t_block b){
-    if (b->next && b->next ->free ){
-        b->size += BLOCK_SIZE + b->next->size;
-        b->next = b->next->next;
-        if (b->next){
-            b->next ->prev = b;
-        }
+struct metadata_block* fusionar_bloques (struct metadata_block* block) {
+  /* Me fijo si el bloque tiene un siguiente y está libre*/
+  if (block->next && block->next ->free ){
+    /* Fusiono ambos bloques */
+    block->size += META_SIZE + block->next ->size;
+    block->next = block->next->next;
+    if (block->next) {
+      /* Pongo al anterior del siguiente como el actual */
+      block->next->prev = block;
     }
-    return (b);
+  }
+
+  return (block);
 }
 
-t_block find_block(t_block *last , size_t size ){
-    t_block b=base;
-    while (b && !(b->free && b->size >= size )) {
-        *last = b;
-        b = b->next;
-    }
-    return (b);
+/* Función encontrar_bloque_libre() */
+struct metadata_block *encontrar_bloque_libre(struct metadata_block **last, size_t size) {
+  /* Recorro la lista desde la base buscando un bloque que este libre
+     y que su tamaño sea mayor o igual al tamaño necesitado  */
+  struct metadata_block *current = base;
+  while (current && !(current->free && current->size >= size)) {
+    *last = current;
+    current = current->next;
+  }
+
+  /* Retorno un bloque si lo encontré o NULL */
+  return current;
 }
 
 /* Función extender_heap() */
-/* Add a new block at the of heap*/
-/* return NULL if things go wrong*/
-t_block extend_heap ( t_block last , size_t s){
-    
-    //printf("aca extender \n");//****%ld   %10p    %10ld ********************!!!!!!
-    
-    void* sb;
-    t_block b;
-    b = sbrk(0);
-    sb = sbrk(BLOCK_SIZE + s);
+struct metadata_block *extender_heap(struct metadata_block* last, size_t size) {
+  /* Creo un nuevo bloque */
+  struct metadata_block *block;
 
-    //printf("    \n\n");//************************!!!!!!
-    if (sb == (void*)-1){
+  /* Obtengo el break y coloco el bloque ahí */
+  block = sbrk(0);
 
-            //printf("aca10 \n");//************************!!!!!!
+  /* Intento aumentar el break en el tamaño requerido,
+     mas el tamaño del bloque de meta-data */
+  void *request = sbrk(size + META_SIZE);
 
-            
-        return (NULL);
-    }
-    b->size = s;
-    b->next = NULL;
-    b->prev = last;
-    b->ptr = b->data;
-    if (last){
-        last->next = b;
-    }
-    b->free = 0;
-    return (b);
+  /* Si no se pudo aumentar el break, retorno NULL, hubo un error */
+  if (request == (void*) -1) {
+    return NULL;
+  }
+
+  /* Si no es la primera vez, actualizo el next del último */
+  if (last) {
+    last->next = block;
+  }
+
+  /* Actualizo el tamaño, el siguiente, el anterior y el estado (free) del nuevo bloque */
+  block->size = size;
+  block->next = NULL;
+  block->prev = last;
+  block->free = 0;
+  /* Retorno el bloque recientemente alocado */
+  return block;
 }
 
+/* Función obtener_puntero_bloque() */
+struct metadata_block *obtener_puntero_bloque(void *ptr) {
+  /* Retorno el puntero al bloque desado */
+  return (struct metadata_block*) ptr - 1;
+}
 
 /* Función print_list() */
 /* Sólo para debugging, imprime la lista doblemente enlazada
    de una forma que sea sencilla de ver e interpretar */
-void print_list() {
-    t_block b = base;
+void imprimir_lista() {
+    struct metadata_block *b = base;
 
     /* Si la lista esta vacia no hago nada */
-    if(b == NULL){
-        return;
-
-        printf("aca \n");//************************!!!!!!
-
-    }
+    if(b == NULL) return;
 
     /* Si no esta vacia, la recorro e imprimo los datos en forma de tabla */
     int index = 1;
 
-    printf("+----------+----------+----------+------------+------------+------------+\n");
-    printf("| %8s | %8s | %8s | %10s | %10s | %10s |\n", "Nº Bloque", "Tamaño", "Libre", "Anterior", "Actual", "Siguiente");
-    printf("+----------+----------+----------+------------+------------+------------+\n");
+    printf("+----------+----------+----------+-----------------------+---------------------+-------------------+\n");
+    printf("| %8s | %8s | %8s | %20s | %20s | %20s |\n", "Nº Bloque", "Tamaño", "Libre", "Anterior", "Actual", "Siguiente");
+    printf("+----------+----------+----------+-----------------------+---------------------+-------------------+\n");
 
     do {
-        printf("| %8d | %8zu | %8s | %10p | %10p | %10p |\n",index, b->size,  (b->free) ? "Si": "No", b->prev, b, b->next);
+        printf("| %8d | %8zu | %8s | %20p | %20p | %20p |\n", index, b->size,  (b->free) ? "Si": "No", b->prev, b, b->next);
         index++;
         b = b->next;
     } while(b != NULL);
 
-    printf("+----------+----------+----------+------------+------------+------------+\n");
+    printf("+----------+----------+----------+-----------------------+---------------------+--------------------+\n");
 
 }
